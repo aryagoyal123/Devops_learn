182982e01d5961a5484b5b2bf084978079338d6c
Yogesh Kumar Goyal <yogesh.goyal@clarivate.com>
2024-11-11 15:55:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+function showMessage() {
+  const message = document.getElementById("message");
+  message.innerText = "Thank you for visiting, Dost!";
+  message.classList.remove("hidden");
+}
